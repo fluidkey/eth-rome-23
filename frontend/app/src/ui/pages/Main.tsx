0d6334d2f47b2cf9de0c3fa70f642f1f3f53abe2
Antonio <antonio.giovanni.seveso@gmail.com>
2023-10-07 20:00:13 +0200

    feat: added graphql endpoints

--- frontend/app/src/ui/pages/Main.tsx
+++ frontend/app/src/ui/pages/Main.tsx
@@ -4,20 +4,56 @@ import { Box, Typography } from "@mui/material";
 import Name from "../organisms/Name/Name";
 import Dashboard from "../organisms/Dashboard/Dashboard";
 import Footer from "../organisms/Footer/Footer";
-import { useQuery } from '@apollo/client';
-import { IS_USER_REGISTERED } from '../../graphql/codegen/queries/User';
+import { useMutation, useQuery } from '@apollo/client';
+import { GET_USER_BY_ADDRESS, IS_USER_REGISTERED } from '../../graphql/codegen/queries/User';
+import { REGISTER_USER, SET_USERNAME } from '../../graphql/codegen/mutations/User';
+import { generatePrivateKey, privateKeyToAccount } from 'viem/accounts';
+import { useEffect } from 'react';
 
 export default function Main() {
   const { address } = useAccount();
   const nameSet = true;
 
-  const {data} = useQuery(IS_USER_REGISTERED, {
-    variables: {
-      address: '0x74C19105f358BAb85f8E9FDA9202A1326A714d89',
-    }
-  });
+  // const {data} = useQuery(IS_USER_REGISTERED, {
+  //   variables: {
+  //     address: '0x74C19105f358BAb85f8E9FDA9202A1326A714d89',
+  //   }
+  // });
+  // console.log(data);
 
-  console.log(data);
+  // const [registerUser, {}] = useMutation(REGISTER_USER, {
+  //   variables: {
+  //     registerUserInput: {
+  //       address: '0x44C19105f358BAb85f8E9FDA9202A1326A714d84',
+  //       spendingPubKey: privateKeyToAccount(generatePrivateKey()).publicKey,
+  //       viewingPubKey: privateKeyToAccount(generatePrivateKey()).publicKey,
+  //     }
+  //   }
+  // });
+  // useEffect(() => {
+  //   registerUser().then((res) => {
+  //     console.log(res);
+  //   });
+  // }, []);
+
+  // const [setUsername, {}] = useMutation(SET_USERNAME, {
+  //   variables: {
+  //     address: '0x44C19105f358BAb85f8E9FDA9202A1326A714d84',
+  //     username: 'testusername',
+  //   }
+  // });
+  // useEffect(() => {
+  //   setUsername().then((res) => {
+  //     console.log(res);
+  //   });
+  // }, []);
+
+  // const {data} = useQuery(GET_USER_BY_ADDRESS, {
+  //   variables: {
+  //     address: '0x44C19105f358BAb85f8E9FDA9202A1326A714d84'
+  //   }
+  // });
+  // console.log(data);
 
   return (
     <>
