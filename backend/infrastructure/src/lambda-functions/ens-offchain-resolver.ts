e058a376a747d983c8ded9c1dd96a80df2a19d5e
Federico Castelli <fc@oracleofde.fi>
2023-10-07 16:20:05 +0200

    feat: working on ens offchain resolver

--- backend/infrastructure/src/lambda-functions/ens-offchain-resolver.ts
+++ backend/infrastructure/src/lambda-functions/ens-offchain-resolver.ts
@@ -1,13 +1,16 @@
 import {APIGatewayProxyEvent, APIGatewayProxyResult} from "aws-lambda";
-// import {SigningKey} from "@ethersproject/signing-key";
+import {SigningKey} from "@ethersproject/signing-key";
+// import { keccak256 } from 'viem';
+// import { toHex } from 'viem/utils';
 // import {Buffer} from "buffer";
-// import * as ethers from "ethers";
-// import { abi as Resolver_abi } from '@ensdomains/ens-contracts/artifacts/contracts/resolvers/Resolver.sol/Resolver.json'
+import * as ethers from "ethers";
+import {hexConcat} from "ethers/lib/utils";
+import { abi as Resolver_abi } from '@ensdomains/ens-contracts/artifacts/contracts/resolvers/Resolver.sol/Resolver.json'
 // import { abi as IResolverService_abi } from '@ensdomains/offchain-resolver-contracts/artifacts/contracts/OffchainResolver.sol/IResolverService.json'
 
-// const PRIVATE_KEY = process.env.PRIVATE_KEY as string;
-// const signer = new SigningKey(PRIVATE_KEY);
-// const resolver = new ethers.utils.Interface(Resolver_abi);
+const PRIVATE_KEY = process.env.PRIVATE_KEY as string;
+const signer = new SigningKey(PRIVATE_KEY);
+const resolver = new ethers.utils.Interface(Resolver_abi);
 
 /*
 function decodeDnsName(dnsname: Buffer) {
@@ -48,8 +51,33 @@ export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGat
     return [result, validUntil, sigData]
   },
   */
+
+  const result = {addr: ['0x74C19105f358BAb85f8E9FDA9202A1326A714d89'], ttl: 1000};
+  // const result = JSON.stringify({});
+  const resultEnccoded = resolver.encodeFunctionResult('addr(bytes32,uint256)', [ result ]);
+  let messageHash = ethers.utils.solidityKeccak256(
+    ['bytes', 'address', 'uint64', 'bytes32', 'bytes32'],
+    [
+      '0x1900',
+      '0xCBDbC3EC4133f489d63A54E8784b1447FC37DE04',
+      0,
+      ethers.utils.keccak256('0x'),
+      ethers.utils.keccak256(resultEnccoded),
+    ]
+  )
+
+  const sig = signer.signDigest(messageHash)
+  const sigData = hexConcat([sig.r, sig.s, new Uint8Array([sig.v])])
+
   return {
     statusCode: 200,
-    body: JSON.stringify({}),
+    body: JSON.stringify({
+      result: result,
+      sigData: sigData,
+      validUntil: 0
+    }),
   };
 }
+
+// @ts-ignore
+lambdaHandler({}).then(() => {});
